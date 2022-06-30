2c52045f694fdea6e4b3865d79b21679ee58e2d5
QuietJarvis <1171677568@qq.com>
2022-06-30 23:20:26 +0800

    提交代码

---
+++ Test001.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()//主函数—程序的入口—mian函数有且只有一个
+{
+	//这里完成任务
+	//在屏幕上输出hello world
+	//函数-print funtion-printlf-打印函数
+	//库函数-C语言本身提供给我们使用的函数
+	//别人的东西-打招呼
+
+	printlf("hello world");
+	return 0;
+}
+
+//int是整型的意思
+//mian前面的int表示main函数调用返回一个整型值（return 0）
